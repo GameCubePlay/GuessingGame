afaa5632ff73e87d4edaec6f20429fdc37bffdc6
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2020-12-04 13:07:18 +0100

    Added infinite loop and break after guess

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,22 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
     println!("The secret number is: {}", secret_number);
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("File to read line");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("File to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Place type a number!");
-    println!("You guessed: {}", guess);
+        let guess: u32 = guess.trim().parse().expect("Place type a number!");
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("To small"),
-        Ordering::Greater => println!("To big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("To small"),
+            Ordering::Greater => println!("To big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
