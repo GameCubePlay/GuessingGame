a6621dcbeba66bd8dbc32e21b54c91443a1533be
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2020-12-04 11:38:06 +0100

    Fix typos

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,13 @@ fn main() {
 
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("File to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Place type a number!");
     println!("You guessed: {}", guess);
 
     match guess.cmp(&secret_number) {
         Ordering::Less => println!("To small"),
-        Ordering::Geater => println!("To big!"),
-        Orderingf::Equal => println!("You win!"),
+        Ordering::Greater => println!("To big!"),
+        Ordering::Equal => println!("You win!"),
     }
 }
